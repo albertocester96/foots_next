eb1956db85381757648380815a277b4fb012656d
albertocester96 <albertocester96@gmail.com>
2024-07-16 12:11:04 +0200

    set-up-env.local

--- foots-next/src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-export default function Login() {
-    return(
-        <div className="flex min-h-screen flex-col justify-center items-center">
-            <h1>
-                Ciao
-            </h1>
-        </div>
-    )
-}
