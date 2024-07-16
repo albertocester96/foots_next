eca1dae114a35356c954d42256fc2dbd0f5c946c
albertocester96 <albertocester96@gmail.com>
2024-07-16 10:58:32 +0200

    daisyui-button-test

--- foots-next/src/components/Button.js
+++ foots-next/src/components/Button.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Button() {
     return(
-        <button className="font-semibold rounded-lg bg-yellow-300 p-3" >
+        <button className="btn sm:btn-sm bg-primary text-white border-none rounded-lg" >
             <Link href="/login">
                 Vai al login
             </Link>
