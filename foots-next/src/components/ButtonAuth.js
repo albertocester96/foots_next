68475abe3f7a9237f8e20ec4a3a68a3faed9d8c9
albertocester96 <albertocester96@gmail.com>
2024-07-25 13:18:23 +0200

    approved status on click doesn't work

--- foots-next/src/components/ButtonAuth.js
+++ foots-next/src/components/ButtonAuth.js
@@ -1,11 +1,23 @@
 import Link from "next/link"
 
-export default function ButtonAuth() {
+export default function ButtonAuth() { //da capire come mai non funziona
     return(
-        <button className="btn sm:btn-sm bg-primary text-white border-none rounded-lg" >
-            <Link href="/autenticazione">
-                Login
-            </Link>
-        </button>
+        <>
+            <div>
+                <title className="js-button-clicked">ciao</title> 
+            </div>
+            <button className="btn sm:btn-sm bg-primary text-white border-none rounded-lg"  
+                    onClick={()=> {
+                        document.querySelector(".js-button-clicked")
+                        .innerHTML = "Approvato"
+                        }
+                    }
+                    >
+                <Link>
+                    Login
+                </Link>
+            </button>
+        </>
     )
 }
+
