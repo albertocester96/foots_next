57ab3703ddbaadde8c4af4dfc138b2f3cb66d4f0
albertocester96 <albertocester96@gmail.com>
2024-08-01 14:18:10 +0200

    set timer on words array

--- foots-next/src/app/page.js
+++ foots-next/src/app/page.js
@@ -1,28 +1,26 @@
+import WordsGenerator from "@/utils/generateWords";
 import ButtonAuth from "../components/ButtonAuth";
 
 export default function Home() {
+
   return (
         <main className="h-screen flex flex-col md:flex-row">
-          <div className="w-full md:w-8/12 h-1/2 md:h-full relative">
-            <img 
-              src="https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?cs=srgb&dl=pexels-christian-heitz-285904-842711.jpg&fm=jpg" 
-              alt="Enhanced landscape"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
+          <div className="flex w-full md:w-3/5 h-1/2 md:h-full relative bg-slate-600 items-center p-5">
+            <WordsGenerator />
           </div>
           
-          <div className="w-full md:w-4/12 h-screen">
+          <div className="w-full md:w-2/5 h-screen">
             <div className="flex flex-col justify-center items-center h-full">
               <h1 className="font-bold text-2xl"> Inizia </h1>
                 <div className="flex flex-row space-x-4 justify-center items-center">
                   <ButtonAuth url="/login" text="Login"/>
                   <ButtonAuth url="/signUp" text="Sign Up"/>
                 </div>
-              <div className="w-4/12 text-center space-y-2 absolute bottom-0 mb-4">
+              <div className="w-4/12 text-center space-y-2 absolute bottom-0 mb-10">
                 <p> logo</p>
                 <div className="flex justify-center font-normal text-slate-400 text-xs underline">
                   <a href="" className="px-2">
-                    condizioni d'uso
+                    Condizioni d'uso
                   </a>
                   <span> | </span>
                   <a href="" className="px-2">
---
+++ foots-next/src/utils/generateWords.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+export default function WordsGenerator() {
+  const [currentWord, setCurrentWord] = useState("");
+
+
+  useEffect(() => {
+    const words = ["ciao", "Alberto"];
+    let index = 0;
+
+     const intervalId = setInterval(() => {
+            setCurrentWord(words[index]);
+            index = (index + 1) % words.length;
+            }, 1000);
+        
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <h1 className='text-white font-bold text-4xl'>
+        {currentWord}
+    </h1>
+  )
+}
